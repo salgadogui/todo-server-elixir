fbc23e75f914fadef62fea5668be3d270e5940c4
Guilherme <guilherme.asalgado@gmail.com>
2024-10-13 14:41:36 -0300

    first commit

---
+++ lib/todo_cache/todo/server.ex
@@ -0,0 +1,38 @@
+defmodule Todo.Server do
+  def start do
+    pid = spawn(fn -> loop(Todo.List.new()) end)
+    {:ok, pid}
+  end
+
+  def loop(todo_list) do
+    new_todo_list =
+      receive do
+        message -> process_message(todo_list, message)
+      end
+
+    loop(new_todo_list)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    send(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    send(todo_server, {:entries, self(), date})
+
+    receive do
+      {:todo_entries, entries} -> entries
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
+
+  defp process_message(todo_list, {:add_entry, new_entry}) do
+    Todo.List.add_entry(todo_list, new_entry)
+  end
+
+  defp process_message(todo_list, {:entries, caller, date}) do
+    send(caller, {:todo_entries, Todo.List.entries(todo_list, date)})
+    todo_list
+  end
+end
